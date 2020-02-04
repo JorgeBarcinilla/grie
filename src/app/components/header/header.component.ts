f8849cd05f2ebd3d46b17cf8657ba4052e3e8329
Jorge Luis Palacio Barcinilla <jpalacio20@cuc.edu.co>
2020-02-04 00:49:05 -0500

    Se agrega un titulo al ingresar a una  vista

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,100 +13,16 @@ import {
 })
 export class HeaderComponent implements OnInit {
 
-  items: MenuItem[];
+   title: string;
 
   constructor() {}
 
   ngOnInit() {
-    this.items = [{
-        label: 'Politica de administracion de riesgos',
-        icon: 'fa fa-fw fa-check',
-        items: [
-          [
-            {
-              items: [{
-                label: 'Lineamientos de la politica',
-                routerLink: ['/lineamientosPolitica']
-              }]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Identificacion del riesgo',
-        icon: 'fa fa-fw fa-soccer-ball-o',
-        items: [
-          [{
-              items: [{
-                label: 'Caracterizacion del contexto',
-                routerLink: ['/caracterizacionContexto']
-              }, {
-                label: 'Identificaacion del  riesgo',
-                routerLink: ['/identificacionRiesgo']
-              }]
-            }
-
-          ]
-        ]
-      },
-      {
-        label: 'Valoracion de riesgos',
-        icon: 'fa fa-fw fa-child',
-        items: [
-          [{
-              items: [{
-                label: 'Analisis de riesgos',
-                routerLink: ['/analisisRiesgo']
-              }, {
-                label: 'Evaluacion de riesgos',
-                routerLink: ['/evaluacionRiesgos']
-              }, {
-                label: 'Reporte del plaan de tratamiento de riesgos',
-                routerLink: ['/reporteTratamientoRiesgo']
-              }]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Preparacion para la respuesta a emergencias',
-        icon: 'fa fa-fw fa-gears',
-        items: [
-          [{
-              items: [{
-                label: 'Ver seccion',
-                routerLink: ['/preparacionRespuestaEmergencia']
-              }]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Ejecucion de la respuesta',
-        icon: 'fa fa-fw fa-gears',
-        items: [
-          [{
-              items: [{
-                label: 'Ver seccion',
-                routerLink: ['/ejecucionRespuesta']
-              }]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Preparación para la recuperación',
-        icon: 'fa fa-fw fa-gears',
-        items: [
-          [{
-              items: [{
-                label: 'Ver seccion',
-                routerLink: ['/preparacionRecuperacion']
-              }]
-            }
-          ]
-        ]
-      }
-    ]
+    
   }
+
+  changeTitle(titleButton: string){
+    this.title = titleButton;
+  }
+
 }
