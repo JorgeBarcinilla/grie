bb5176721e7617dccf1b76816f891eb136b39679
Jorge Luis Palacio Barcinilla <jpalacio20@cuc.edu.co>
2020-03-09 23:12:22 -0500

    Se termina la vista identificacion institucion

--- src/app/views/conocimiento-institucional/opcion1/identificacion-institucion/identificacion-institucion.component.ts
+++ src/app/views/conocimiento-institucional/opcion1/identificacion-institucion/identificacion-institucion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-institucion',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificacionInstitucionComponent implements OnInit {
 
+  formIdentificacionInstitucional = new FormGroup({
+    nombre : new FormControl('', Validators.required),
+    codDane : new FormControl('', Validators.required),
+    rector : new FormControl('', Validators.required),
+    coordinador : new FormControl('', Validators.required),
+    liderPRAE : new FormControl(''),
+    liderLogistica : new FormControl(''),
+    liderConsejoAcademico : new FormControl(''),
+    liderBrigada : new FormControl(''),
+    liderPadres : new FormControl(''),
+    liderEstudiantes : new FormControl(''),
+    representanteOrganismosSocorro : new FormControl(''),
+    mision : new FormControl(''),
+    vision : new FormControl(''),
+    objetivos : new FormControl(''),
+    valores : new FormControl(''),
+    sedes : new FormControl('', Validators.required),
+  })
+
+  formSede = new FormGroup({
+    nombre: new FormControl('',Validators.required)
+  })
+
+  listaSedes = []
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  agregarSede(){
+    this.listaSedes.push(this.formSede.value)
+    this.formIdentificacionInstitucional.get('sedes').setValue(this.listaSedes)
+    this.formSede.reset()
+  }
+
+  eliminarSede(sede){
+    this.listaSedes = this.listaSedes.filter( element => {return element != sede});
+    this.formIdentificacionInstitucional.get('sedes').setValue(this.listaSedes)
+  }
+
+  guardarIdentificacionInstitucion(){
+    console.log(this.formIdentificacionInstitucional.value)
   }
 
 }
--- src/app/views/conocimiento-institucional/opcion1/identificacion-institucion/identificacion-institucion.module.ts
+++ src/app/views/conocimiento-institucional/opcion1/identificacion-institucion/identificacion-institucion.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { IdentificacionInstitucionRoutingModule } from './identificacion-institucion-routing.module';
 import { IdentificacionInstitucionComponent } from './identificacion-institucion.component';
+import { AppMaterialModule } from 'src/app/shared/angular-material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CardTitleModule } from 'src/app/components/utils/card-title/card-title.module';
+import { ButtonExampleModule } from 'src/app/components/utils/button-example/button-example.module';
 
 
 @NgModule({
   declarations: [IdentificacionInstitucionComponent],
   imports: [
     CommonModule,
-    IdentificacionInstitucionRoutingModule
+    IdentificacionInstitucionRoutingModule,
+    AppMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CardTitleModule,
+    ButtonExampleModule
   ]
 })
 export class IdentificacionInstitucionModule { }
